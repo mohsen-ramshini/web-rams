6f7d84307244a7ba4f03c8db705bf60eed657062
mohsen-ramshini <rsmohsen20@gmail.com>
2025-04-25 10:50:41 +0330

    Install all necessary packages

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,jsx,ts,tsx}', // تنظیم برای جستجوی فایل‌های JS و JSX در پوشه src
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
